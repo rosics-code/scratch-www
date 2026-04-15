900bd1bcfa5c7fb3dd51128fcd2f5d3770518a99
rosics-code <roblox888i12345@gmail.com>
2026-04-15 18:08:41 -0400

    Simplify membership link rendering
    Removed conditional rendering for membership link when not logged in.

--- src/components/navigation/www/navigation.jsx
+++ src/components/navigation/www/navigation.jsx
@@ -123,18 +123,15 @@ class Navigation extends React.Component {
                             <FormattedMessage id="general.ideas" />
                         </a>
                     </li>
-                    {
-                        this.props.isLoggedIn === false &&
-                            (
-                                <li className="link membership">
+                    
+                    <li className="link membership">
                                     <a
                                         href="/membership"
                                     >
                                         <FormattedMessage id="general.membership" />
                                     </a>
-                                </li>
-                            )
-                    }
+                    </li>
+                            
 
                     <li className="search">
                         <Form onSubmit={this.handleSearchSubmit}>
